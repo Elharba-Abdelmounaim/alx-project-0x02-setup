a3a95ea468c642bc053623c3715df96e8717da23
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-14 21:30:33 +0100

    Scaffold Next.js project with TypeScript and Tailwind

--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Home() {
-  return (
+export default function Home(){
+  return(
     <>
       <Head>
-        <title>Welcome</title>
+        <title>WELCOME</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Welcome to ALX Next.js Project!
+      <main className="flex min-h-screen flex-col items-center justify-center" >
+        <h1 className="text-4xl font-bold text-blue-500">
+          Welcome to ALX next.js Project!
         </h1>
       </main>
     </>
