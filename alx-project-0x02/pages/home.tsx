4f1b99494ab1adcf451233291c49b72b6f1290f1
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-14 22:21:03 +0100

    Create reusable Card component and use it in home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,8 +1,8 @@
-import Card from '../components/common/Card'
+import Card from '@/components/common/Card'  // أو المسار النسبي الصحيح حسب مشروعك
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center space-y-6 p-4 bg-red-100">
+    <main className="flex min-h-screen flex-col items-center justify-center space-y-6 p-4 bg-gray-100">
       <Card title="Card 1" content="This is the content of card one." />
       <Card title="Card 2" content="This is some different content for card two." />
       <Card title="Card 3" content="Here is yet another card with unique content." />
